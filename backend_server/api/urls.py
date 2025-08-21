3b2aa8c04a87ac86f5ed74e24df01da9c438e605
Jannah Shayne <jnnhshn12@gmail.com>
2025-08-21 23:18:18 +0800

    maim

--- backend_server/api/urls.py
+++ backend_server/api/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('api/kutsero/', include('api.kutsero.urls')), 
     path('api/login/', views.login, name='login'),  # Add api/ here
     path('api/signup/', views.signup, name='signup'),
     path('api/get_data/', views.get_data, name='get_data'),
     path('api/insert_vet_profile/', views.insert_vet_profile, name='insert_vet_profile'),
+    
     
 ]
 
