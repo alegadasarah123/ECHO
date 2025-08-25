a37cc56aeb70b7d671a17604d7a878ae62865fd9
Jannah Shayne <jnnhshn12@gmail.com>
2025-08-25 18:00:02 +0800

    mai

--- backend_server/backend/urls.py
+++ backend_server/backend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('api/kutsero/', include('api.kutsero.urls')),  
     path('', include('api.urls')),
     path("api/kutsero_president/", include("api.kutsero_president.urls")),
-    path('api/horse_operator/', include('api.horse_operator.urls')), 
+    path('api/horse_operator/', include('api.horse_operator.urls')),
+    path('ctu_vetmed/', include('api.ctu_vetmed.urls')),
 
 ]
